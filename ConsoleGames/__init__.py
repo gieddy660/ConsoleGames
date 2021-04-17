dc4970f40ac3cbb326456d27f9acecb75f2fb7f6
gieddy660 <gieddy660@gmail.com>
2021-04-17 16:12:00 +0200

    small refactoring

--- ConsoleGames/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ['ConsoleGraphic', 'Stuff']
--- consolegames/graphics.py
+++ consolegames/graphics.py
@@ -1,18 +1,18 @@
 import os
-from ConsoleGames._WinConsole import set_position
+from consolegames._win import set_position
 
 
 class Console:
     def __init__(self, size_x, size_y, textures, mode=2, times_x=1, griglia=None):
         self.size_x = size_x
         self.size_y = size_y
-        self.griglia = [[None for _ in range(size_x)] for __ in range(size_y)]
+        self.grid = [[None for _ in range(size_x)] for __ in range(size_y)]
         self.textures = textures
         self.times_x = times_x
         if griglia is not None:
             self.size_x = len(griglia[0])
             self.size_y = len(griglia)
-            self.griglia = griglia
+            self.grid = griglia
 
         if os.name in ('nt', 'dos'):
             def clear(): a = os.system('cls')
@@ -35,20 +35,20 @@ class Console:
 
     def refresh_un(self):
         self.clear()
-        for row in self.griglia:
+        for row in self.grid:
             for cell in row:
                 print(self.textures[cell] * self.times_x, end='')
             print()
 
-    def refresh(self, new_griglia):
+    def refresh(self, new_grid):
         for y in range(self.size_y):
             for x in range(self.size_x):
-                if self.griglia[y][x] != new_griglia[y][x]:
+                if self.grid[y][x] != new_grid[y][x]:
                     set_position(x*self.times_x, y)
-                    print(self.textures[new_griglia[y][x]]*self.times_x)
-        self.griglia = self._copy(new_griglia)
+                    print(self.textures[new_grid[y][x]] * self.times_x)
+        self.grid = self._copy(new_grid)
 
     def __getitem__(self, item):
         if isinstance(item, int) and item < 0:
             raise IndexError('no negative indexes')
-        return self.griglia[item]
+        return self.grid[item]
